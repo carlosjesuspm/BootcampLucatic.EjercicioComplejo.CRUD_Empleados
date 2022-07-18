da795d0c797f66c69cb6a44965d905a3e99f8799
carlosjesuspm8 <permarcar96@gmail.com>
2022-07-18 11:18:17 +0200

    Departamento

--- src/main/java/com/cj/empleados01/modelo/Departamento.java
+++ src/main/java/com/cj/empleados01/modelo/Departamento.java
@@ -1,5 +1,67 @@
 package com.cj.empleados01.modelo;
 
+import java.util.Objects;
+
 public class Departamento {
     
+    String nombreDepartamento;
+    String codigoDepartamento; 
+
+    public Departamento() {
+    }
+
+    public Departamento(String nombreDepartamento, String codigoDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+        this.codigoDepartamento = codigoDepartamento;
+    }
+
+    public String getNombreDepartamento() {
+        return this.nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public String getCodigoDepartamento() {
+        return this.codigoDepartamento;
+    }
+
+    public void setCodigoDepartamento(String codigoDepartamento) {
+        this.codigoDepartamento = codigoDepartamento;
+    }
+
+    public Departamento nombreDepartamento(String nombreDepartamento) {
+        setNombreDepartamento(nombreDepartamento);
+        return this;
+    }
+
+    public Departamento codigoDepartamento(String codigoDepartamento) {
+        setCodigoDepartamento(codigoDepartamento);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Departamento)) {
+            return false;
+        }
+        Departamento departamento = (Departamento) o;
+        return Objects.equals(nombreDepartamento, departamento.nombreDepartamento) && Objects.equals(codigoDepartamento, departamento.codigoDepartamento);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombreDepartamento, codigoDepartamento);
+    }
+
+    @Override
+    public String toString() {
+        return
+            "Nombre del Departamento= " + getNombreDepartamento() + "\n" +
+            "Código del Departamento= " + getCodigoDepartamento();
+    }
+
 }
