1e2ac190bd0857a52ff6229311d9b9bbdc9c0911
carlosjesuspm8 <permarcar96@gmail.com>
2022-07-18 11:57:54 +0200

    Capa gui

---
+++ src/main/java/com/cj/empleados01/gui/Menu.java
@@ -0,0 +1,27 @@
+package com.cj.empleados01.gui;
+
+public class Menu {
+    
+    public void mostrarMenu(){
+
+        System.out.println("");
+        System.out.println("-------------------------");
+        System.out.println("-------------------------");
+        System.out.println("LISTADO EMPLEADOS EMPRESA");
+        System.out.println("-------------------------");
+        System.out.println("-------------------------");
+        System.out.println("");
+        System.out.println("Opciones del menú: ");
+        System.out.println("1. Añadir empleado");
+        System.out.println("2. Añadir gerente");
+        System.out.println("3. Añadir director");
+        System.out.println("4. Modificar empleado/gerente/director");
+        System.out.println("5. Eliminar empleado");
+        System.out.println("6. Ver listado completo de empleados");
+        System.out.println("7. Comisionar gerente");
+        System.out.println("8. Comisionar director");
+        System.out.println("0. Salir del menú");
+        System.out.println("");
+
+    }
+}
